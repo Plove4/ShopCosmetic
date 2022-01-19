57a99b911a20873862f72f0259eafb3d93c8dfb3
Никита <mamichevnikita@gmail.com>
2022-01-19 12:59:18 +0300

    Подключение базы данных к проекту Создание класса Frame

---
+++ ShopCosmetic/AppDateFile/FrameOdj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ShopCosmetic.AppDateFile
+{
+    class FrameOdj
+    {
+        public static Frame frameMain;
+    }
+}
--- ShopCosmetic/MainWindow.xaml.cs
+++ ShopCosmetic/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ShopCosmetic.AppDateFile;
 
 namespace ShopCosmetic
 {
@@ -23,6 +24,8 @@ namespace ShopCosmetic
         public MainWindow()
         {
             InitializeComponent();
+            ConnectoOdb.conObj = new KosmeticDBEntities();
+            FrameOdj.frameMain = frmMain;
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
